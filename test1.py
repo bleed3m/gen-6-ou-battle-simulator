b006954eb4955504747c6b124649e7d78b460145
Bleedem <jjacobwow@gmail.com>
2024-03-14 17:48:26 -0400

    update

--- test1.py
+++ test1.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import pandasql as ps
 
-data = pd.read_csv("pokemon.csv")
-data = data.set_index("id")
-print(data.query("type2 == 'Flying'"))
+pokemon = pd.read_csv("pokemon.csv")
+pokemon = pokemon.set_index("id")
+# print(pokemon.query("type2 == 'Flying'"))
+
+sql_query = "SELECT * FROM pokemon WHERE name == 'Gliscor'"
+# print(ps.sqldf(sql_query, locals()))
+
+# print(pokemon.head(5))
+
+# Read Headers
+# print(pokemon.columns)
+
+# Read each column
+print(pokemon[['name', 'type1']][0:2])
+print(pokemon[['name']])
+
+# Reach each row
+print(pokemon.head(2))
+print(pokemon.iloc[1]) # int location
+for i, r in pokemon.iterrows():
+    print(i, r)
+print(pokemon.loc[pokemon['type1'] == "Ground"])
+ 
+# Read a specific location (R, C)
+print(pokemon.iloc[0, 0])
